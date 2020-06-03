709d57e25dfa0b079698e9d96f3854b27c0fcdd2
Administrator <zhangcaiqi888@126.com>
2020-06-03 17:12:38 +0800

    增加调用

--- app/src/main/java/com/zy/usbgpslib/MainActivity.java
+++ app/src/main/java/com/zy/usbgpslib/MainActivity.java
@@ -6,9 +6,53 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView receiveText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        receiveText = findViewById(R.id.receive_text);
+        UsbGpsService.getInstance().start(this, new UsbGpsListener() {
+                    @Override
+                    public void onFailed(String msg) {
+                        Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT);
+                    }
+
+                    @Override
+                    public void onRead(GpsInfo gpsInfo) {
+                        SpannableStringBuilder spn = new SpannableStringBuilder();
+                        spn.append(gpsInfo.getLatitudeDirection()+":");
+                        spn.append(gpsInfo.getLatitude()+"\r\n");
+                        receiveText.append(spn);
+                    }
+
+                    @Override
+                    public void onUsbAttached() {
+                        SpannableStringBuilder spn = new SpannableStringBuilder();
+                        spn.append("usb设备已插入\r\n");
+                        receiveText.append(spn);
+                    }
+
+                    @Override
+                    public void onUsbDetached() {
+                        SpannableStringBuilder spn = new SpannableStringBuilder();
+                        spn.append("usb设备已拔出\r\n");
+                        receiveText.append(spn);
+                    }
+                }
+        );
+    }
+
+    @Override
+    protected void onDestroy() {
+        UsbGpsService.getInstance().onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        super.onNewIntent(intent);
     }
 }
